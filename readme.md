557ef2de32f62f905faeefbd1b3d6f2145e35510
Smerth <merth@me.com>
2018-03-06 13:55:56 -0800

    Logger setup

---
+++ readme.md
@@ -0,0 +1,7 @@
+# React Lifecycles - lifecycle-logger App
+
+See the tutorial [React Lifecycles](https://www.lynda.com/React-js-tutorials/React-Lifecycles/592509-2.html)
+
+## Releases
+
+* Release 1.0.0 - Logger setup
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from './App'
+import registerServiceWorker from "./registerServiceWorker";
+import { LoggerExample } from "./loggerExample";
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<LoggerExample />, document.getElementById("root"));
+registerServiceWorker();
