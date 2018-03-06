b0c7f91731b23e0d15d21ef8bc7dd92a3572f86a
Smerth <merth@me.com>
2018-03-06 15:29:35 -0800

    displayName

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  static displayName = "SomethingNew";
+
   render() {
     return <h1>Hello</h1>;
   }
 }
-
-function myTestWrapper(WrappedComponent) {
-  return class extends Component {
-    render() {
-      return (
-        <div
-          style={{
-            backgroundColor: "blue"
-          }}
-        >
-          <WrappedComponent />
-        </div>
-      );
-    }
-  };
-}
-
-App = myTestWrapper(App);
 
 export default App;
